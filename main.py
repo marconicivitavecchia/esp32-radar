97f77e11267e761097aa32042de26085c69eb9d5
Sebastiano Melita <sebastiano.melita@gmail.com>
2024-07-20 15:36:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -256,13 +256,13 @@ def leggi_reboot():
 # Map of the functions to be executed on a certain path of the received commands (statuses).
 # They must coincide with the corresponding paths of the JSON object being transmitted.
 # Read-only commands are parameterless and can be invoked in JSON as cells in a command list. For example, with JSON
-# "radar": [polltime, servel] 
+# "radar": ["polltime", "servel"] 
 # but they must be stored as field-value pairs of an object because in Python dictionary arrays are encoded as objects.
 # Write-only commands are parameterized and must be invoked in JSON as field, value pairs. For example, with JSON
 # "radar": {
 # 	"write":{
-# 		polltime: 1
-# 		servel: 115200
+# 		"polltimev: 1
+# 		"servel": 115200
 # 	},
 # }
 command_map = {
