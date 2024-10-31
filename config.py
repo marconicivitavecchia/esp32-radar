f65301f578b04f123df9e31d84f60dbef2fdd518
Sebastiano Melita <sebastiano.melita@gmail.com>
2024-10-31 12:02:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
-WIFI_SSID1 = "WebPocket-E280"
-WIFI_PASSWORD1 = "dorabino.7468!"
-WIFI_SSID2 = "sensori"
-WIFI_PASSWORD2 = "sensori2019"
-#WIFI_SSID = "sensori"
-#WIFI_PASSWORD = "sensori2019"
+WIFI_SSID1 = "xxxxxxxxx1"
+WIFI_PASSWORD1 = "yyyyyyyy1"
+WIFI_SSID2 = "xxxxxx2"
+WIFI_PASSWORD2 = "yyyyyyy2"
 MQTT_CLIENT_ID = "radar-"
 # The main broker is the preferred broker
 # The backup broker is choosen only when the main broker is unavailable
 # If the backup broker is active, the main broker is periodically tested and
 # selected if again avalilable
 # The same behaviour is applied by the IoT device
-MQTT_BROKER1 = "proxy.marconicloud.it"
-MQTT_BROKER2 = "broker.emqx.io"
+MQTT_BROKER1 = "mqtt1.example.com"
+MQTT_BROKER2 = "mqtt2.example.com"
 NTP_SERVER = "3.pool.ntp.org"
 MQTT_USER = ""
 MQTT_PASSWORD = ""
